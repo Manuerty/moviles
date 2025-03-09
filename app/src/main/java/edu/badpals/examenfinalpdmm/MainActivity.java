e9def05ed8f88c3138c15f314bb4e83f98b47d44
Manuerty <manuelcendonr@gmail.com>
2025-03-09 17:35:16 +0100

    set up master

--- app/src/main/java/edu/badpals/examenfinalpdmm/MainActivity.java
+++ app/src/main/java/edu/badpals/examenfinalpdmm/MainActivity.java
@@ -1,11 +1,12 @@
 package edu.badpals.examenfinalpdmm;
 
-import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-import edu.badpals.examenfinalpdmm.activities.ListadoAnimales;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +14,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, ListadoAnimales.class);
-        startActivity(intent);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
